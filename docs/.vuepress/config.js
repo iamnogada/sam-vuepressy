114219fa56e8b6ffc66b9c0b939273135796b332
muguddy <seungil.han@gmail.com>
2019-01-25 14:52:29 +0900

    test01

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'Learning vuepress',
+    description: 'Sample project to explain how vuepress works',
+    head: [
+        ['link',{rel: 'icon', href: 'www.naver.com'}],
+    ]
+}
--- docs/README.md
+++ docs/README.md
@@ -3,3 +3,5 @@
 ### Comments
 
 > Hello
+
+![이미지](images/2019-01-25-11-38-06.png)
